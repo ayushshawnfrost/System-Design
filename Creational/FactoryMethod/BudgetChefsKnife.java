6fa883bd3263a59bfbcc6baeb8b433f8aea59434
Ayush Sharma <ayushmarkevens@gmail.com>
2024-09-14 13:25:16 -0700

    Create BudgetChefsKnife.java

---
+++ Creational/FactoryMethod/BudgetChefsKnife.java
@@ -0,0 +1,17 @@
+// Concrete Product: BudgetChefsKnife
+public class BudgetChefsKnife extends Knife {
+    @Override
+    public void sharpen() {
+        System.out.println("Sharpening Budget Chef's Knife");
+    }
+
+    @Override
+    public void polish() {
+        System.out.println("Polishing Budget Chef's Knife");
+    }
+
+    @Override
+    public void pack() {
+        System.out.println("Packing Budget Chef's Knife");
+    }
+}
