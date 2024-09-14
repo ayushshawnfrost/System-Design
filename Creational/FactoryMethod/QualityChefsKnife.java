9e33a08dd0650a038083a613e9ee49a466472abc
Ayush Sharma <ayushmarkevens@gmail.com>
2024-09-14 13:25:42 -0700

    Create QualityChefsKnife.java

---
+++ Creational/FactoryMethod/QualityChefsKnife.java
@@ -0,0 +1,17 @@
+// Concrete Product: QualityChefsKnife
+public class QualityChefsKnife extends Knife {
+    @Override
+    public void sharpen() {
+        System.out.println("Sharpening Quality Chef's Knife");
+    }
+
+    @Override
+    public void polish() {
+        System.out.println("Polishing Quality Chef's Knife");
+    }
+
+    @Override
+    public void pack() {
+        System.out.println("Packing Quality Chef's Knife");
+    }
+}
